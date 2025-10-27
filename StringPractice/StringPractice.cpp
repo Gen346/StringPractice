8062f2f879f9782752da4455cffc00bdca81d2f9
Gen346 <kelzreez@gmail.com>
2025-10-27 23:49:12 +0200

    Added two methods ToLower() and ToUpper()

--- StringPractice/StringPractice.cpp
+++ StringPractice/StringPractice.cpp
@@ -1,5 +1,5 @@
-﻿#include <iostream>
-#include <cstring>
+﻿#include <cstring>
+#include <iostream>
 #include <string>
 
 
@@ -13,6 +13,42 @@ std::string StringReverseCpp(const std::string& stringToReverse)
 	}
 
 	return reverseString;
+}
+
+std::string ToUpper(const std::string& stringToUpper)
+{
+	std::string upperString(stringToUpper.size(), ' ');
+
+	for (int i = 0; i < stringToUpper.size(); ++i)
+	{
+		if ((int)stringToUpper[i] > 96 && (int)stringToUpper[i] < 123)
+		{
+			upperString[i] = (int)stringToUpper[i] - 32;
+		}
+		else
+		{
+			upperString[i] = stringToUpper[i];
+		}
+	}
+	return upperString;
+}
+
+std::string ToLower(const std::string& stringToLower)
+{
+	std::string lowerString(stringToLower.size(), ' ');
+
+	for (int i = 0; i < stringToLower.size(); ++i)
+	{
+		if ((int)stringToLower[i] > 64 && (int)stringToLower[i] < 91)
+		{
+			lowerString[i] = (int)stringToLower[i] + 32;
+		}
+		else
+		{
+			lowerString[i] = stringToLower[i];
+		}
+	}
+	return lowerString;
 }
 
 char* StringReverseC(char* stringToReverse)
@@ -40,11 +76,21 @@ char* StringReverseC(char* stringToReverse)
 int main()
 {
 
-	std::cout << "Enter a string to reverse: ";
-	
+	/*std::cout << "Enter a string to reverse: ";
+
 	std::string input = "";
 
 	std::getline(std::cin, input);
 
-	std::cout << StringReverseCpp(input);
+	std::cout << StringReverseCpp(input);*/
+
+	/*std::cout << "Enter a string to upper: ";
+	std::string input = " ";
+	std::getline(std::cin, input);
+	std::cout << ToUpper(input) << std::endl;*/
+
+	std::cout << "Enter a string to upper: ";
+	std::string input = " ";
+	std::getline(std::cin, input);
+	std::cout << ToLower(input) << std::endl;
 }
