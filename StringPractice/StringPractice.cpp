cb95bb004dca9da8de81fdafbf20c4dae9a09bc3
Gen346 <kelzreez@gmail.com>
2025-10-27 21:53:39 +0200

    First push

---
+++ StringPractice/StringPractice.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstring>
+
+std::string StringReverseCpp(const std::string& stringToReverse)
+{
+	std::string reverseString(stringToReverse.size(), ' ');
+
+	for (int i = 0; i < stringToReverse.size(); ++i)
+	{
+		reverseString[i] = stringToReverse[stringToReverse.size() - i];
+	}
+
+	return reverseString;
+}
+
+char* StringReverseC(char* stringToReverse)
+{
+	int stringSize = strlen(stringToReverse);
+
+	char* start = stringToReverse;
+
+	char* end = stringToReverse + stringSize - 1;
+
+	while (start < end)
+	{
+		char temp = *start;
+		*start = *end;
+		*end = temp;
+	}
+
+	return stringToReverse;
+}
+
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
